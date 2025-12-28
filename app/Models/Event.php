65090cdf738e800032d0ce9cc54522caa2132215
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2025-12-28 23:59:40 +0200

    Adding, Editing, Deleting Events by postman

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'name', 'description', 'start_time', 'end_time'];
     public function user()
     {
         return $this->belongsTo(User::class);
